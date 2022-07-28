3b7a10d1ca03175dcb6324b0eb08fc2db7379a00
Dr Nic Williams <drnicwilliams@gmail.com>
2022-07-28 10:40:08 +1000

    Look up $STACK to pick which build to use
    https://devcenter.heroku.com/articles/buildpack-api\#stacks

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 # set -x
 set -e
 
-docker build -t heroku-buildpack-imagemagick-webp .
+VERSION=${1:-22}
+
+docker build -t heroku-buildpack-imagemagick-webp -f "Dockerfile.$VERSION" .
 mkdir -p build
 
 docker run --rm -t -v $PWD/build:/data heroku-buildpack-imagemagick-webp sh -c 'cp -f /usr/src/imagemagick/build/*.tar.gz /data/'

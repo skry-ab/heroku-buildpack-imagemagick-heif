2c69bd18317b53e7fb4a9fa80a8a1e1fb6c2d3f9
Albert Hafvenström <albhaf@gmail.com>
2024-12-13 22:21:53 +0100

    build: bump all versions for 22

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 
 VERSION=${1:-22}
 
-docker build -t heroku-buildpack-imagemagick-webp -f "Dockerfile.$VERSION" .
+docker build --platform linux/amd64 -t heroku-buildpack-imagemagick-webp -f "Dockerfile.$VERSION" .
 mkdir -p build
 
-docker run --rm -t -v $PWD/build:/data heroku-buildpack-imagemagick-webp sh -c 'cp -f /usr/src/imagemagick/build/*.tar.gz /data/'
+docker run --platform linux/amd64 --rm -t -v $PWD/build:/data heroku-buildpack-imagemagick-webp sh -c 'cp -f /usr/src/imagemagick/build/*.tar.gz /data/'
